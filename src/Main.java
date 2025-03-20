fc1bdcb7deeac137f4480126465c454b4a199eed
CristianD312 <cristianfdisanto@gmail.com>
2025-03-20 18:22:48 -0300

    Creacion
    Ingreso el primero modelo del algoritmo

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main (String[] args){
+
+        Persona perso = new Persona();
+
+        System.out.println("El nombre de la persona es "+perso.getNombre());
+
+        Persona perso2 = new Persona("Juan","Gutierrez","10/05/1990",35,38934567);
+        System.out.println("El nombre de la persona es "+perso2.getNombre());
+        Persona perso3 = new Persona("Miguel","Diaz","10/05/1990",35,38934567);
+        System.out.println("El nombre de la persona es "+perso3.getNombre());
+        perso.setNombre("Jose");
+        System.out.println("--------------------------------------------");
+        System.out.println("El nombre de la persona es "+perso.getNombre());
+        System.out.println("El nombre de la persona es "+perso2.getNombre());
+        System.out.println("El nombre de la persona es "+perso3.getNombre());
+        System.out.println(perso2.toString());
+
+
+
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,69 @@
+public class Persona {
+    public String nombre;
+    String apellido;
+    String fechaNacimiento;
+    int edad;
+    int dni;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String fechaNacimiento, int edad, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ", edad=" + edad +
+                ", dni=" + dni +
+                '}';
+    }
+}
